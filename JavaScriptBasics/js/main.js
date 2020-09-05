52b612f0c31a617d2b2134e921a6d5b211ca2abd
Y-YOSHIMU <mcu7.oldtr.uc@gmail.com>
2020-09-05 23:12:04 +0900

    [Finish]#10まで

---
+++ JavaScriptBasics/js/main.js
@@ -0,0 +1,71 @@
+  'use strict'
+
+// console.log('hello');
+// console.log("hello");
+
+// console.log("it's me!");
+// console.log('it\'s me!');
+
+// \nで改行、\tでtab
+// console.log('hel\nlo wor\tld');
+
+// console.log('hello' + 'world');
+
+
+
+// #06
+// console.log(10 + 3); // 13
+// console.log(10 - 3); // 7
+// console.log(10 * 3); // 30
+// console.log(10 / 3); // 3.3333
+// console.log(10 % 3); // 1
+// console.log(10 ** 3); // 1000
+
+// console.log(2 + 10 * 3); // 32
+// console.log((2 + 10) * 3); // 36
+
+
+// #07 定数 const
+
+
+// console.log(150 * 140);
+// console.log(150 * 160);
+
+// let price = 150;
+// console.log(price * 140);
+// console.log(price * 160);
+
+// #08 変数 let var
+
+// price = 170;
+// console.log(price * 140);
+// console.log(price * 160);
+
+
+// #09
+
+// let price = 500;
+
+// // price = price + 100;
+// price += 100;  //600
+
+// // price = price * 2;
+// price *= 2; // 1200
+
+// // price = price + 1;
+// // price += 1;
+// price++; // 1201
+
+// // price -= 1;
+// price--; // 1200
+
+// console.log(price);
+
+
+// #10 データ型
+
+console.log(typeof 'hello');
+console.log(typeof 5);
+console.log(typeof true);
+console.log(typeof undefined);
+console.log(typeof null);
