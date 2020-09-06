b056f65221091f542388cf9b9929dfbab2f9c000
Y-YOSHIMU <mcu7.oldtr.uc@gmail.com>
2020-09-06 16:44:47 +0900

    [Finish]#20まで

--- JavaScriptBasics/js/main.js
+++ JavaScriptBasics/js/main.js
@@ -64,8 +64,165 @@
 
 // #10 データ型
 
-console.log(typeof 'hello');
-console.log(typeof 5);
-console.log(typeof true);
-console.log(typeof undefined);
-console.log(typeof null);
+// console.log(typeof 'hello');
+// console.log(typeof 5);
+// console.log(typeof true);
+// console.log(typeof undefined);
+// console.log(typeof null);
+
+
+// #11
+// console.log('5' * 3);
+// console.log('5' - '3');
+
+// console.log('5' + 3); // 文字列の結合としての+
+// console.log(parseInt('5', 10) + 3); // 10進数の整数値に変換
+ // console.log(parseInt('hello', 10)); //not a number NaN
+
+
+// #12 比較演算子を使ってみよう
+
+// const price = 1200;
+
+// console.log(price > 1000); //true
+// console.log(price < 1000); //false
+// console.log(price >= 1000); //true
+// console.log(price <= 1000); //false
+// console.log(price === 1000); //false
+// console.log(price !== 1000); //true
+
+// false <- 0, null, undefined, '', false
+// true <- それ以外
+
+// console.log(Boolean(0));
+// console.log(Boolean('hello'));
+
+
+// #13 ifで条件分岐をしてみよう
+
+// const score =40;
+
+// if (score >= 80) {
+//   console.log('Great!');
+// } else if (score >= 60) {
+//   console.log('Good.');
+// } else {
+//   console.log('OK...');
+// }
+
+
+// #14 条件演算子を使ってみよう
+
+// const score = 90;
+
+// score >= 80 ? console.log('Great!') : console.log('OK...');
+
+
+// #15 論理演算子を使ってみよう
+
+// const score = 60;
+// const name = 'taguchi';
+
+// // if (score >= 50) {
+// //   if(name === 'taguchi') {
+// //     console.log('Good job!');
+// //   }
+// // }
+
+// // && なおかつ (AND)
+// // || もしくは (OR)
+// // !  ~ではない (NOT)
+
+// if (score >= 50 && name === 'taguchi') {
+//   console.log('Good job!');
+// }
+
+
+// #16 switchで条件分岐をしてみよう
+
+// const signal = 'pink';
+
+// // if (signal === 'red') {
+// //   console.log('Stop!');
+// // } else if (signal === 'yellow') {
+// //   console.log('Caution!');
+// // } else if (signal === 'blue') {
+// //   console.log('Go!');
+// // }
+
+// switch (signal) {
+//   case 'red':
+//     console.log('Stop!');
+//     break;
+//   case 'yellow':
+//     console.log('Caution!');
+//     break;
+//   case 'blue':
+//   case 'green':
+//     console.log('Go!');
+//     break;
+//   default:
+//     console.log('Wrong signal!');
+//     break;
+// }
+
+
+
+// #17 forを使ったループ処理をしよう
+
+// for (let i = 1; i <= 10; i++) {
+//   // console.log('hello');
+//   // console.log('hello' + i);
+//   console.log(`hello ${i}`);  //バッククオート`` shift + @
+// }
+
+
+
+// #18 whileを使ったループ処理をしよう
+
+// let hp = 100;
+
+// while (hp > 0) {
+//   console.log(`${hp} HP left!`);
+//   hp -= 15;
+// }
+
+// let hp = -50;
+
+// do {
+//   console.log(`${hp} HP left!`);
+//   hp -= 15;
+// } while (hp > 0);
+
+
+
+// #19 continue,breakを使ってみよう
+
+// for (let i = 1; i <= 10; i++) {
+//   // if (i === 4) {
+//   //   if (i % 3 === 0) {
+//   //   continue;
+//   // }
+//   if (i === 4) {
+//     break;
+//   }
+//   console.log(i);
+// }
+
+
+
+// #20 関数で処理をまとめてみよう
+
+function showAd() {
+  console.log('------------');
+  console.log('-----Ad-----');
+  console.log('------------');
+}
+
+showAd();
+console.log('Tom is great!');
+console.log('Bob is great!');
+showAd();
+console.log('Steve is great!');
+console.log('Richard is great!');
+showAd();
