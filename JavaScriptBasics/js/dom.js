5d80599ebd9e16eed253e07ea58319bbfd440669
Y-YOSHIMU <mcu7.oldtr.uc@gmail.com>
2020-09-12 23:40:22 +0900

    [Finish]#08

--- JavaScriptBasics/js/dom.js
+++ JavaScriptBasics/js/dom.js
@@ -5,15 +5,21 @@
 // #05 addEventListener()を使ってみよう
 // #06 要素の属性を操作してみよう
 // #07 classNameを操作してみよう
+// #08 classListを使ってみよう
 
 {
 
   document.querySelector('button').addEventListener('click', () => {
     const targetNode = document.getElementById('target')
 
-    // targetNode.className = 'my-color';
-    targetNode.className = 'my-color my-border';
-
+    // targetNode.className = 'my-color my-border';
+    // targetNode.classList.add('my-color');
+    // if (targetNode.classList.contains('my-color') === true) {
+    //   targetNode.classList.remove('my-color');
+    // } else {
+    //   targetNode.classList.add('my-color');
+    // }
+    targetNode.classList.toggle('my-color');
   });
 
 }
