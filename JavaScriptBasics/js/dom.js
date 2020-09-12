19b6e8c2c18e6342b8e4c0a4fe5043abd0b40f57
Y-YOSHIMU <mcu7.oldtr.uc@gmail.com>
2020-09-12 23:08:37 +0900

    [Finish]#03

--- JavaScriptBasics/js/dom.js
+++ JavaScriptBasics/js/dom.js
@@ -1,11 +1,15 @@
 'use strict';
 // #02 要素を操作してみよう
+// #03 複数の要素を取得してみよう
 
 {
   function update() {
-    // document.querySelector('h1').textContent = 'Changed!';
-    // document.querySelector('#target').textContent = 'Changed!';
-    document.getElementById('target').textContent = 'Changed!';
+    // document.getElementById('target').textContent = 'Changed!';
+    // document.querySelector('p').textContent = 'Changed!'; //一番最初の要素のみ
+    // document.querySelectorAll('p')[1].textContent = 'Changed!'; //[]で指定した要素を変更
+    document.querySelectorAll('p').forEach((p, index) => {
+    p.textContent = `${index}番目のpです！`;
+    });
   }
 
   setTimeout(update, 1000);
