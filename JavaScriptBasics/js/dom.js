e60d907277efcdf61aa013d19279d1b68f05087e
Y-YOSHIMU <mcu7.oldtr.uc@gmail.com>
2020-09-12 23:26:52 +0900

    [Finish]#06

--- JavaScriptBasics/js/dom.js
+++ JavaScriptBasics/js/dom.js
@@ -3,11 +3,17 @@
 // #03 複数の要素を取得してみよう
 // #04 要素の取得方法を理解しよう
 // #05 addEventListener()を使ってみよう
+// #06 要素の属性を操作してみよう
 
 {
 
   document.querySelector('button').addEventListener('click', () => {
-    document.getElementById('target').textContent = 'Changed!';
+    const targetNode = document.getElementById('target')
+
+    targetNode.textContent = 'Changed!';
+    targetNode.title = 'This is title!';
+    targetNode.style.color = 'red';
+    targetNode.style.backgroundColor = 'skyblue'; //background-color ハイフンではなくCamelで！
   });
 
 }
