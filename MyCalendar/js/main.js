07cd74508c748e60942f67e758045d837c56b233
Y-YOSHIMU <mcu7.oldtr.uc@gmail.com>
2020-09-27 21:43:12 +0900

    [Fin]#12

--- MyCalendar/js/main.js
+++ MyCalendar/js/main.js
@@ -61,8 +61,15 @@ console.clear();
       ...getCalendarBody(),
       ...getCalendarTail(),
     ];
+    const weeks = [];
+    const weeksCount = dates.length / 7;
 
-    console.log(dates);
+    for (let i = 0; i < weeksCount; i++) {
+      weeks.push(dates.splice(0, 7));
+    }
+
+    // console.log(dates);
+    console.log(weeks);
   }
 
   createCalendar();
