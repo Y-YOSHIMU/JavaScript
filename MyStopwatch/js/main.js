cbd370705238cebc7a6de1db1c06ce992b006bd2
Y-YOSHIMU <mcu7.oldtr.uc@gmail.com>
2020-10-01 17:12:39 +0900

    [Complete]Stopwatch

--- MyStopwatch/js/main.js
+++ MyStopwatch/js/main.js
@@ -6,9 +6,9 @@
   const stop = document.getElementById('stop');
   const reset= document.getElementById('reset');
 
-let startTime;
-let timeoutId;
-let elapsedTime = 0;
+  let startTime;
+  let timeoutId;
+  let elapsedTime = 0;
 
   function countUp() {
     const d = new Date(Date.now() - startTime + elapsedTime);
@@ -23,38 +23,47 @@ let elapsedTime = 0;
   }
 
   function setButtonStateInitial() {
-    start.disabled = false;
-    stop.disabled = true;
-    reset.disabled = true;
+    start.classList.remove('inactive');
+    stop.classList.add('inactive');
+    reset.classList.add('inactive');
   }
 
   function setButtonStateRunning() {
-    start.disabled = true;
-    stop.disabled = false;
-    reset.disabled = true;
+    start.classList.add('inactive');
+    stop.classList.remove('inactive');
+    reset.classList.add('inactive');
   }
 
   function setButtonStateStopped() {
-    start.disabled = false;
-    stop.disabled = true;
-    reset.disabled = false;
+    start.classList.remove('inactive');
+    stop.classList.add('inactive');
+    reset.classList.remove('inactive');
   }
 
   setButtonStateInitial();
 
   start.addEventListener('click', () => {
+    if (start.classList.contains('inactive') === true) {
+      return;
+    }
     setButtonStateRunning();
     startTime = Date.now();
     countUp();
   });
 
   stop.addEventListener('click', () => {
+    if (stop.classList.contains('inactive') === true) {
+      return;
+    }
     setButtonStateStopped();
     clearTimeout(timeoutId);
     elapsedTime += Date.now() - startTime;
   });
 
   reset.addEventListener('click', () => {
+    if (reset.classList.contains('inactive') === true) {
+      return;
+    }
     setButtonStateInitial();
     timer.textContent = '00:00.000';
     elapsedTime = 0;
