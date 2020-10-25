a24fa73be8b177d347484fcbc0d59e6447b37f67
Y-YOSHIMU <mcu7.oldtr.uc@gmail.com>
2020-10-25 23:05:01 +0900

    [Fin]#05

--- Canvas/js/main.js
+++ Canvas/js/main.js
@@ -10,11 +10,15 @@
 
     ctx.fillStyle = 'pink';
     ctx.strokeStyle = '#f00';
-    ctx.lineWidth = 8;
-    // ctx.lineJoin = 'round';
-    ctx.lineJoin = 'bevel';
+
     ctx.fillRect(50, 50, 50, 50);
     ctx.strokeRect(50, 50, 50, 50);
+
+    ctx.fillStyle = 'skyblue';
+    ctx.strokeStyle = '#00b';
+
+    ctx.fillRect(70, 70, 50, 50);
+    ctx.strokeRect(70, 70, 50, 50);
   }
 
   draw();
