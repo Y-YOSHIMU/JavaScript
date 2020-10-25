a00019ec7c02524124a197fa2888fa1c3f5f5418
Y-YOSHIMU <mcu7.oldtr.uc@gmail.com>
2020-10-25 22:55:10 +0900

    [Fin]#03

--- Canvas/js/main.js
+++ Canvas/js/main.js
@@ -2,7 +2,15 @@
 
 {
   function draw() {
+    const canvas = document.querySelector('canvas');
+    if (typeof canvas.getContext === 'undefined') {
+      return;
+    }
+    const ctx = canvas.getContext('2d');
 
+    // ctx.fillRect(x, y, width, height);
+    // ctx.fillRect(50, 50, 50, 50);
+    ctx.strokeRect(50, 50, 50, 50);
   }
 
   draw();
