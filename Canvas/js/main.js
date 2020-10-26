f4deba5fa99829bcda62115e614d014fc0257837
Y-YOSHIMU <mcu7.oldtr.uc@gmail.com>
2020-10-26 22:16:06 +0900

    [Fin]#08

--- Canvas/js/main.js
+++ Canvas/js/main.js
@@ -8,24 +8,12 @@
     }
     const ctx = canvas.getContext('2d');
 
-    // ctx.createLinearGradient(x0, y0, x1, y1);
-    // const g = ctx.createLinearGradient(0, 0, canvas.width, 0);
-    // const g = ctx.createRadialGrandient(
-    //     x0, y0, r0,
-    //     x1, y1, r1
-    //   );
-    const g = ctx.createRadialGradient(
-        canvas.width / 2, canvas.height /2, 50,
-        canvas.width / 2 + 100, canvas.height /2 + 200, 500
-      );
+    ctx.shadowOffsetX = 4;
+    ctx.shadowOffsetY = 4;
+    ctx.shadowBlur = 4;
+    ctx.shadowColor = 'rgba(0, 0, 0, 0.3)';
 
-    g.addColorStop(0, '#f00');
-    g.addColorStop(0.1, '#0f0');
-    g.addColorStop(1, '#00f');
-
-    ctx.fillStyle = g;
-    // ctx.fillRect(0, 0, 600, 240);
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillRect(50, 50, 50, 50);
   }
 
   draw();
