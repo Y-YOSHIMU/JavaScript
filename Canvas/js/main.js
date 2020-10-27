8fc6927090dd48791f612000d9e2e5bf475ff96c
Y-YOSHIMU <mcu7.oldtr.uc@gmail.com>
2020-10-27 23:01:40 +0900

    [Fin]#17

--- Canvas/js/main.js
+++ Canvas/js/main.js
@@ -8,34 +8,37 @@
     }
     const ctx = canvas.getContext('2d');
 
-    const img = document.createElement('img');
-    img.src = 'img/sprite.png';
+    ctx.fillStyle = 'pink';
+    ctx.fillRect(0, 0, 200, 200);
 
-    img.addEventListener('load', () => {
+    ctx.beginPath();
+    ctx.ellipse(100, 100, 40, 30, 0, 0, 2 * Math.PI);
+    ctx.fillStyle = 'black';
+    ctx.fill();
 
-      ctx.beginPath();
-      ctx.ellipse(100, 100, 40, 30, 0, 0, 2 * Math.PI);
-      ctx.fillStyle = 'black';
-      ctx.fill();
+    ctx.beginPath();
+    ctx.ellipse(80, 100, 8, 8, 0, 0, 2 * Math.PI);
+    ctx.ellipse(120, 100, 8, 8, 0, 0, 2 * Math.PI);
+    ctx.fillStyle = 'skyblue';
+    ctx.fill();
 
-      ctx.beginPath();
-      ctx.ellipse(80, 100, 8, 8, 0, 0, 2 * Math.PI);
-      ctx.ellipse(120, 100, 8, 8, 0, 0, 2 * Math.PI);
-      ctx.fillStyle = 'skyblue';
-      ctx.fill();
+    ctx.scale(0.5, 0.5);
+    ctx.translate(400, 0);
+    ctx.rotate(45 / 180 * Math.PI);
 
-      ctx.scale(0.5, 0.5);
-      ctx.beginPath();
-      ctx.ellipse(100, 100, 40, 30, 0, 0, 2 * Math.PI);
-      ctx.fillStyle = 'black';
-      ctx.fill();
+    ctx.fillStyle = 'olive';
+    ctx.fillRect(0, 0, 200, 200);
 
-      ctx.beginPath();
-      ctx.ellipse(80, 100, 8, 8, 0, 0, 2 * Math.PI);
-      ctx.ellipse(120, 100, 8, 8, 0, 0, 2 * Math.PI);
-      ctx.fillStyle = 'skyblue';
-      ctx.fill();
-    });
+    ctx.beginPath();
+    ctx.ellipse(100, 100, 40, 30, 0, 0, 2 * Math.PI);
+    ctx.fillStyle = 'black';
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.ellipse(80, 100, 8, 8, 0, 0, 2 * Math.PI);
+    ctx.ellipse(120, 100, 8, 8, 0, 0, 2 * Math.PI);
+    ctx.fillStyle = 'skyblue';
+    ctx.fill();
   }
 
   draw();
