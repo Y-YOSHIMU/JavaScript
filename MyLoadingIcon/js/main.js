2f7ac91fbc5b8a84164ec37ff4ec8209969bbd31
Y-YOSHIMU <mcu7.oldtr.uc@gmail.com>
2020-11-01 22:46:06 +0900

    [Fin]#5

--- MyLoadingIcon/js/main.js
+++ MyLoadingIcon/js/main.js
@@ -7,19 +7,19 @@
       this.width = canvas.width;
       this.height = canvas.height;
       this.r = 60;
+
+      this.angle = 0;
     }
 
     draw() {
       this.ctx.translate(this.width / 2, this.height / 2);
+      this.ctx.rotate(Math.PI / 180 * this.angle);
 
       this.ctx.beginPath();
-      // this.ctx.arc(this.width / 2, this.height / 2, this.r, 0, 2 * Math.PI);
       this.ctx.arc(0, 0, this.r, 0, 2 * Math.PI);
       this.ctx.stroke();
 
       this.ctx.beginPath();
-      // this.ctx.moveTo(this.width / 2, this.height / 2 - this.r - 5);
-      // this.ctx.lineTo(this.width / 2, this.height / 2 - this.r + 5);
       this.ctx.moveTo(0, - this.r - 5);
       this.ctx.lineTo(0, - this.r + 5);
       this.ctx.strokeStyle = 'orange';
@@ -27,8 +27,17 @@
       this.ctx.stroke();
     }
 
+    update() {
+      this.angle += 12;
+    }
+
     run() {
+      this.update();
       this.draw();
+
+      setTimeout(() => {
+        this.run();
+      }, 100);
     }
   }
 
