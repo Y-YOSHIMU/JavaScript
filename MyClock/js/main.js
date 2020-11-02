b60f9e67e3266fd956da9fb677c0616d72ae171f
Y-YOSHIMU <mcu7.oldtr.uc@gmail.com>
2020-11-02 23:42:27 +0900

    [Fin]#03

--- MyClock/js/main.js
+++ MyClock/js/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// #2 Clockクラスを作ろう
+// #3 盤面を描く準備をしよう
 {
   class Clock {
     drawFace() {
@@ -12,6 +12,16 @@
 
       const width = canvas.width;
       const height = canvas.height;
+
+      for (let angle = 0; angle < 360; angle += 6) {
+        ctx.save();
+
+        ctx.translate(width / 2, height / 2);
+        // ctx.rotate(2 * Math.PI / 360 * angle); 約分
+        ctx.rotate(Math.PI / 180 * angle);
+
+        ctx.restore();
+      }
     }
     run() {
       this.drawFace();
