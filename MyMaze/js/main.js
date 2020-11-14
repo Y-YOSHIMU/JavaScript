3f9041a73350bad3972d81c9a0b9bb3054464d25
Y-YOSHIMU <mcu7.oldtr.uc@gmail.com>
2020-11-15 02:42:28 +0900

    [Complete]Maze

--- MyMaze/js/main.js
+++ MyMaze/js/main.js
@@ -6,7 +6,26 @@
       this.ctx = canvas.getContext('2d');
       this.WALL_SIZE = 10;
     }
+
+    render(data) {
+      canvas.height = data.length * this.WALL_SIZE;
+      canvas.width = data.length * this.WALL_SIZE;
+
+      for (let row = 0; row < data.length; row++) {
+        for (let col = 0; col < data[0].length; col++) {
+          if (data[row][col] === 1) {
+            this.ctx.fillRect(
+              col * this.WALL_SIZE,
+              row * this.WALL_SIZE,
+              this.WALL_SIZE,
+              this.WALL_SIZE
+            );
+          }
+        }
+      }
+    }
   }
+
   class Maze {
     constructor(row, col, renderer) {
       if (row < 5 || col < 5 || row % 2 === 0 || col % 2 === 0) {
@@ -79,18 +98,7 @@
     }
 
     render() {
-      for (let row = 0; row < this.data.length; row++) {
-        for (let col = 0; col < this.data[row].length; col++) {
-          if (this.data[row][col] === 1) {
-            this.ctx.fillRect(
-              col * this.WALL_SIZE,
-              row * this.WALL_SIZE,
-              this.WALL_SIZE,
-              this.WALL_SIZE
-            );
-          }
-        }
-      }
+      this.renderer.render(this.data);
     }
   }
 
@@ -99,6 +107,6 @@
     return
   }
 
-  const maze = new Maze(21, 15, new MazeRenderer(canvas);
+  const maze = new Maze(21, 15, new MazeRenderer(canvas));
   maze.render();
 })();
